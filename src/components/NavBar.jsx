9b2ad9a11b7e748926488a31430e6132203ae668
Jelena Skiljevic <skiljevicj@gmail.com>
2022-09-20 21:00:46 +0200

    Nav

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
+import {BsFillCartFill} from "react-icons/bs";
+import logo from '../images/logo.jpg';
 
-
-const NavBar = ({cartNum}) => {
+const NavBar = () => {
   return (
     <div className='navBar'>
-      
-      
+        <img class="logo" src={logo} alt=""  style={{ tintColor: "black"}}/>
+        <a href="">Scandinavian Candy</a>
+        <a className="cart-items">
+        <BsFillCartFill className="icon-cart"  />
+        <div className="cart-num">0</div>
+      </a>
+
     </div>
   )
 }
