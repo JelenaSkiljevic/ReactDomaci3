ae439662f8667c6daad2965b7e0df71c3a13ef9d
Jelena Skiljevic <skiljevicj@gmail.com>
2022-09-20 21:03:56 +0200

    main

---
+++ src/components/Main.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+export const Main = () => {
+  return (
+    <div className='main'>
+        
+        <div className='content'>
+            <h1>Scandinavian Candy</h1>
+            <h3>SHOP NOW</h3>
+        </div>
+    </div>
+  )
+}
