32d38d21649b5c74bd80450f423c7b2025679646
Jelena Skiljevic <skiljevicj@gmail.com>
2022-09-20 18:27:56 +0200

    osnovno ugnjezdavanje

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+const NavBar = ({cartNum}) => {
+  return (
+    <div className='navBar'>
+      
+      
+    </div>
+  )
+}
+
+export default NavBar
---
+++ src/components/Products.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import OneProduct from './OneProduct'
+
+function Products() {
+  return (
+    <div>
+        <OneProduct></OneProduct>
+    </div>
+  )
+}
+
+export default Products
